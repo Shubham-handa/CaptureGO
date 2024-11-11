20d752fb09dfdefa42716233e603d8d1098b1fb3
Shubham-handa <49878873+Shubham-handa@users.noreply.github.com>
2024-11-11 23:44:28 +0530

    Initial Commit

---
+++ CaptureGO/CaptureGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureGOApp.swift
+//  CaptureGO
+//
+//  Created by Shubham Handa on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CaptureGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
